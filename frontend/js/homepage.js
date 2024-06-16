ce05353292a19f8b01df52ab337c65c2e2abcbea
armsaroundmyknees <70464171+armsaroundmyknees@users.noreply.github.com>
2024-06-16 21:57:37 +0700

    add content ch2 pg1,2,3 (effect isnt done yet), make homepage in index.html, move index.html to comic.html

---
+++ frontend/js/homepage.js
@@ -0,0 +1,8 @@
+/********************* click start reading ************************/
+document.querySelector(".readComic").addEventListener("click", () => {
+  if (this.location.hostname === "127.0.0.1") {
+    this.location.replace("/comic.html");
+  } else {
+    this.location.replace("/comic");
+  }
+});
