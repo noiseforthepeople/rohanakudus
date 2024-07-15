1ba5be20f33c9dce77d24e97754ba858c6a8c56c
armsaroundmyknees <70464171+armsaroundmyknees@users.noreply.github.com>
2024-07-15 20:40:21 +0700

    update homepage and rohanna eyes moving

--- frontend/js/homepage.js
+++ frontend/js/homepage.js
@@ -14,3 +14,13 @@ document.querySelector(".continueReadComic").addEventListener("click", () => {
     this.location.replace("/comic");
   }
 });
+
+const rohanaPupils = document.querySelector(".homepageCharacterMoving");
+
+window.addEventListener("mousemove", (e) => {
+  // get x and y postion of cursor
+  var rect = rohanaPupils.getBoundingClientRect();
+  var x = (e.pageX - rect.left) / 130 + "px";
+  var y = (e.pageY - rect.top) / 130 + "px";
+  rohanaPupils.style.transform = "translate3d(" + x + "," + y + ", 0px)";
+});
